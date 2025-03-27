16ed5382e4df7bc7662a981b3409f1ae7bd41363
LucasCatozo <catozolucas@gmail.com>
2025-03-27 19:54:32 -0300

    Finalizado

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,12 @@
+//API key
+const options = {
+    method: 'GET', 
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDljYTA5YTNjMmMyNTY3ZjMzNmU3OGQ2MjI5MjQwMyIsIm5iZiI6MTc0MjUxMjI1MS43ODMsInN1YiI6IjY3ZGNhMDdiMzUwODkxMTQ1MjZiYTlkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MPm0dGLGnh4q7e2cfwLMFhk04BzswCTs4-DuJqDFbo8'
+    }
+}
+
 // Exibir loading 
 function toggleLoading() { 
     let loader = document.querySelector(".loader"); 
